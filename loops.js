525c1a60a121c5554d0d21a49f7248a1930736d4
Ryan Hardin <ryanwhardin@gmail.com>
2020-02-20 20:59:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,11 +17,6 @@ function whileLoop(num){
 }
 
 function doWhileLoop(num){
-  var i = 0;
-  function incrementVariable() {
-    i = i + 1;
-    return i;
-  }
   do{
   console.log('I run once regardless.')
 }while(incrementVariable()<num){
